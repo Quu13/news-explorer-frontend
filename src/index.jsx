9181a4238a62d03789b4097b4109b9aa46a34dd1
Benicen <Benicensoto@Gmail.com>
2025-04-27 22:43:45 -0400

    updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 // src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App';
-import './styles/index.css'; // Or any global CSS you need
+import App from './App'; // <- Correct, because App is in src/
+import './styles/index.css'; // <- If you have styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
