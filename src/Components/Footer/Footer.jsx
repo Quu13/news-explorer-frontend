5bb6c8b46c202fc45c63b2f073365e845339d9a7
Benicen <Benicensoto@Gmail.com>
2025-06-09 20:24:12 -0400

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,19 +11,24 @@ const Footer = () => {
         © {currentYear} Supersite, Powered by News API
       </p>
       <nav className="footer__nav">
-        <a href="/" className="footer__link">Home</a>
-        <a href="https://tripleten.com" target="_blank" className="footer__link">TripleTen</a>
-        <a href="https://github.com/Quu13" target="_blank" className="footer__icon-link">
-          <img src={github} alt="GitHub" className="footer__icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/benicen-soto-b00b30203/" target="_blank" className="footer__icon-link">
-          <img src={linkedIn} alt="LinkedIn" className="footer__icon" />
-        </a>
+        <div className="footer__link-container">
+          <a href="/" className="footer__link">Home</a>
+          <a href="https://tripleten.com" target="_blank" className="footer__link">TripleTen</a>
+        </div>
+        <div className="footer__social-container">
+          <a href="https://github.com/Quu13" target="_blank" className="footer__icon-link">
+            <img src={github} alt="GitHub" className="footer__icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/benicen-soto-b00b30203/" target="_blank" className="footer__icon-link">
+            <img src={linkedIn} alt="LinkedIn" className="footer__icon" />
+          </a>
+        </div>
       </nav>
     </footer>
   );
 };
 
 export default Footer;
+
 
 
