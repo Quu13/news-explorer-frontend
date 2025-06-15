942d97d89037c11b6113869ee7e9e217ab30e6b5
Benicen <Benicensoto@Gmail.com>
2025-06-15 10:51:39 -0400

    updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import App from "./Components/App/App";
+import Main from "./Components/Main/Main";
+import SavedNews from "./Components/SavedNews/SavedNews";
+import ProtectedRoute from "./Components/ProtectedRoute";
+
 import "./index.css";
+
+function AppRouter() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Main />} />
+          <Route
+            path="saved-news"
+            element={<ProtectedRoute element={<SavedNews />} />}
+          />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppRouter />
   </React.StrictMode>
 );
